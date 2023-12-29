464b0694a0db63f95409764dd87d73b79cbb2090
OksanaKorsun <oksana2081989@gmail.com>
2023-12-29 17:59:43 +0200

    trash

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,38 +1,13 @@
-import { configureStore, combineReducers} from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactsSlice';
-// import storage from 'redux-persist/lib/storage';
-import { filterReducer } from './filterSlice';
-// import persistStore from "redux-persist/es/persistStore";
-// import persistReducer from "redux-persist/es/persistReducer";
 
-// import {
-//   FLUSH,
-//   REHYDRATE,
-//   PAUSE,
-//   PERSIST,
-//   PURGE,
-//   REGISTER,
-// } from 'redux-persist';
-// const persistConfig = {
-//   key: 'root',
-//   storage,
-//   whitelist: ['contacts'],
-// };
+import { filterReducer } from './filterSlice';
+
 const rootReducer = combineReducers({
   contacts: contactsReducer,
   filter: filterReducer,
 });
-// const persistedRootReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  // contacts: contactsReducer,
-  // filter: filterReducer,
   reducer: rootReducer,
-  // middleware: getDefaultMiddleware =>
-  //   getDefaultMiddleware({
-  //     serializableCheck: {
-  //       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-  //     },
-  //   }),
 });
-// export const persistor = persistStore(store);
